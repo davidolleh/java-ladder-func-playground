5781a34bd7338e964126895b09080214138873de
davidolleh <asherolleh@gmail.com>
2024-10-31 02:20:47 +0900

    <FEAT> value object로 변경, 예외처리 추가

--- src/main/java/domain/Person.java
+++ src/main/java/domain/Person.java
@@ -1,9 +1,35 @@
 package domain;
 
+import java.util.Objects;
+
 public class Person {
     private final String name;
+    private static final int MAX_LENGTH = 5;
 
     public Person(String name) {
+        nameValidation(name);
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void nameValidation(String name) {
+        if (name.length() > MAX_LENGTH || name.isEmpty())
+            throw new IllegalArgumentException("사람 이름은 1글자 이상 5글자 이하여야 합니다.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
