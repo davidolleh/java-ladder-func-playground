9b5c7b5d2762ed4b60b2abe2ae2b0e76a211edc9
davidolleh <asherolleh@gmail.com>
2024-10-31 02:21:07 +0900

    <FEAT> 사다리 결과 출력 기능 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,6 +5,9 @@ import domain.*;
 import java.util.List;
 
 public class OutputView {
+    public void printEmptyLine() {
+        System.out.println();
+    }
 
     public void printParticipantInquiry() {
         System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
@@ -18,15 +21,34 @@ public class OutputView {
         System.out.println("최대 사다리 높이는 몇 개인가요?");
     }
 
-    public void printLadderResult(Ladder ladder) {
+    public void printResult(List<Person> participants, Ladder ladder, List<Prize> prizes) {
+        System.out.println("사다리 결과");
+
+        printParticipantsResult(participants);
+
+        printLadderResult(ladder);
+
+        printPrizesResult(prizes);
+    }
+
+    private void printParticipantsResult(List<Person> participants) {
+        for (Person participant : participants) {
+            String name = participant.getName();
+            String blanks = " ".repeat(5 - name.length());
+            System.out.print(blanks + name + " ");
+        }
+        System.out.println();
+    }
+
+    private void printLadderResult(Ladder ladder) {
         List<Line> lines = ladder.getLines();
-        int ladderWidth = lines.size();
-        int lineHeight = lines.get(0).getPoints().size();
+        int ladderWidth = ladder.getLadderWidth();
+        int lineHeight = ladder.getLineHeight();
 
         for (int i = 0; i < lineHeight; i++) {
+            System.out.print("    ");
             for (int j = 0; j < ladderWidth; j++) {
                 Direction direction = lines.get(j).getPoints().get(i);
-
                 if (direction == Direction.RIGHT) {
                     System.out.print("|-----");
                 } else {
@@ -35,5 +57,18 @@ public class OutputView {
             }
             System.out.println();
         }
+    }
+
+    private void printPrizesResult(List<Prize> prizes) {
+        for (Prize prize : prizes) {
+            String name = prize.costToString();
+            String blanks = " ".repeat(5 - name.length());
+            System.out.print(blanks + name + " ");
+        }
+        System.out.println();
+    }
+
+    public void printPersonPrizeResult() {
+
     }
 }
