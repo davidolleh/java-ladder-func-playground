125810f1df13bd1b967824929daf2a37b6958096
davidolleh <asherolleh@gmail.com>
2024-10-31 02:22:12 +0900

    <FEAT> Prize vo로 변경

--- src/main/java/domain/Prize.java
+++ src/main/java/domain/Prize.java
@@ -1,9 +1,32 @@
 package domain;
 
+import java.util.Objects;
+
 public class Prize {
     private final int cost;
 
     public Prize(int cost) {
         this.cost = cost;
+    }
+
+    public String costToString() {
+        if (cost ==0)
+            return "꽝";
+
+        return String.valueOf(cost);
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Prize prize = (Prize) o;
+        return cost == prize.cost;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(cost);
     }
 }
