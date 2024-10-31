77f88a7c9ce847dd15c6024ccf546c0e339e1920
davidolleh <asherolleh@gmail.com>
2024-10-31 22:35:13 +0900

    <FEAT> controller 기능 추가

--- src/main/java/LadderGameController.java
+++ src/main/java/LadderGameController.java
@@ -1,6 +1,4 @@
-import domain.Ladder;
-import domain.Person;
-import domain.Prize;
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
@@ -17,22 +15,74 @@ public class LadderGameController {
     }
 
     public void ladderGame() {
-        outputView.printParticipantInquiry();
-        List<Person> participants = inputView.readParticipants();
-
-        outputView.printResultPrizeInquiry();
-        List<Prize> prizes = inputView.readPrizeResults();
-
+        List<Person> participants = readParticipants();
+        List<Prize> prizes = readPrizes();
         if (participants.size() != prizes.size()) {
             throw new IllegalArgumentException("The number of participants does not match the number of prizes.");
         }
 
+        int height = readLadderHeight();
+
+        Ladder ladder = new Ladder(height, participants.size(), new RandomLadderGeneratorImpl());
+//        Map<Person, Prize> participantPrizes = Statistic.statisticResult(ladder, participants, prizes);
+
+        outputView.printResult(participants, ladder, prizes);
+        outputView.printEmptyLine();
+
+        Statistic statistic = new Statistic(ladder, participants, prizes);
+
+
+        while(true) {
+            String name = readParticipantName();
+
+            if (name.equals("all")) {
+                break;
+            }
+
+            Person person = new Person(name);
+            checkParticipantValidation(participants, person);
+            outputView.printParticipantPrizeResult(statistic.getParticipantPrize(person));
+        }
+
+
+        outputView.printParticipantsPrizesResult(statistic.getParticipantPrize(), participants);
+    }
+
+    private List<Person> readParticipants() {
+        outputView.printParticipantInquiry();
+        List<Person> participants = inputView.readParticipants();
+        outputView.printEmptyLine();
+
+        return participants;
+    }
+
+    private List<Prize> readPrizes() {
+        outputView.printResultPrizeInquiry();
+        List<Prize> prizes = inputView.readPrizeResults();
+        outputView.printEmptyLine();
+
+        return prizes;
+    }
+
+    private int readLadderHeight() {
         outputView.printLadderHeightInquiry();
         int height = inputView.readLadderHeight();
+        outputView.printEmptyLine();
 
-        Ladder ladder = new Ladder(height, participants.size());
+        return height;
+    }
 
-        outputView.printLadderResult(ladder);
+    private String readParticipantName() {
+        outputView.printParticipantPrizeInquiry();
+        String name = inputView.readPersonName();
+        outputView.printEmptyLine();
 
+        return name;
+    }
+
+    private void checkParticipantValidation(List<Person> participants, Person person) {
+        if (!participants.contains(person)) {
+            throw new IllegalArgumentException("참여자가 아닙니다.");
+        }
     }
 }
